ee59f8c9610c20afb58943d03282bd184453c96c
HiKris1801 <k.komissarova1801@gmail.com>
2025-04-06 10:58:03 -0400

    add test_data

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,19 @@
 from hexlet_pytest.example import reverse
+from pathlib import Path
 
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / 'test_data' / filename
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+def test_reverse_from_file():
+    original = read_file('before.txt')
+    expected = read_file('after.txt')
+    assert reverse(original).strip() == expected.strip()
